02a25127958f280e7eadca67de29337fdbe25c30
Vamshi51282 <135943611+Vamshi51282@users.noreply.github.com>
2025-02-02 13:25:07 +0530

    Add files via upload

---
+++ 7Nov copy.js
@@ -0,0 +1,7 @@
+document.getElementById("siginup").addEventListener("submit",(event)=>{
+        event.preventDefault()
+    let usernamevalid= /^[a-zA-Z]{3,}$/
+    let emailvalid=/^[a-zA-Z0-9.+_%$#]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+
+    )}
